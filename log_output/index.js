9b633c588ab54b7214d4b0905fa07a62774c4b46
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2025-10-17 21:26:59 +0300

    1.7 ingres

--- log_output/index.js
+++ log_output/index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 const createHash = (() => {
   const fourRnm = Math.random().toString(36).substr(2, 4);
   return `${Math.random().toString(10).substr(2, 4)}${fourRnm}-${fourRnm}-${fourRnm}-${fourRnm}-${Math.random().toString(36).substr(2, 12)}${Math.random().toString(36).substr(2, 1)}`;
@@ -10,3 +14,13 @@ const printHash = () => {
 
 printHash();
 
+app.get('/status', (req, res) => {
+  res.json({
+    timestamp: new Date().toISOString(),
+    hash: createHash
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
